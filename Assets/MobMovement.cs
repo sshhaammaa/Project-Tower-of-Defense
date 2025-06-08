25b47d4d905abf9569458990035f1126066cc9b9
sshhaammaa <terazahominec@gmail.com>
2025-06-08 05:18:38 +0300

    sthrht
    trshrhdrh

--- Assets/MobMovement.cs
+++ Assets/MobMovement.cs
@@ -5,74 +5,73 @@ using UnityEngine.AI;
 
 public class MobMovement : MonoBehaviour
 {
-    [SerializeField] private NavMeshAgent navMeshAgent;
-    [SerializeField] private int hp;
-    [SerializeField] private int coinsToDie;
+	[SerializeField] private NavMeshAgent navMeshAgent;
+	[SerializeField] private int hp;
+	[SerializeField] private int coinsToDie;
 
-    private bool isDead = false;
+	private bool isDead = false;
 
-    void Start()
-    {
-        if (navMeshAgent == null)
-        {
-            navMeshAgent = GetComponent<NavMeshAgent>();
-        }
+	void Start()
+	{
+		if (navMeshAgent == null)
+		{
+			navMeshAgent = GetComponent<NavMeshAgent>();
+		}
 
-        StartCoroutine(InitializeMovement());
-    }
+		StartCoroutine(InitializeMovement());
+	}
 
-    private IEnumerator InitializeMovement()
-    {
-        // Затримка на 1 кадр (можна більше, якщо треба)
-        yield return null;
+	private IEnumerator InitializeMovement()
+	{
+		yield return null;
 
-        if (navMeshAgent != null && navMeshAgent.isOnNavMesh)
-        {
-            navMeshAgent.SetDestination(navmesh.Instance.Finish.position);
-            navMeshAgent.avoidancePriority = Random.Range(1, 100);
-        }
-        else
-        {
-            Debug.LogWarning($"{gameObject.name} не на NavMesh! Не вдалося встановити ціль.");
-        }
-    }
+		if (navMeshAgent != null && navMeshAgent.isOnNavMesh)
+		{
+			navMeshAgent.SetDestination(navmesh.Instance.Finish.position);
+			navMeshAgent.avoidancePriority = Random.Range(1, 100);
+		}
+		else
+		{
+			Debug.LogWarning($"{gameObject.name} не на NavMesh! Не вдалося встановити ціль.");
+		}
+	}
 
 
-    void Update()
-    {
-        Vector3 direction = navMeshAgent.velocity;
-        if (direction != Vector3.zero)
-        {
-            Quaternion toRotation = Quaternion.LookRotation(direction);
-            transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, navMeshAgent.angularSpeed * Time.deltaTime);
-        }
-    }
+	void Update()
+	{
+		Vector3 direction = navMeshAgent.velocity;
+		if (direction != Vector3.zero)
+		{
+			Quaternion toRotation = Quaternion.LookRotation(direction);
+			transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, navMeshAgent.angularSpeed * Time.deltaTime);
+		}
+	}
 
-    public bool GetDamage(int damage)
-    {
-        if (isDead) return false; 
+	public bool GetDamage(int damage)
+	{
+		if (isDead) return false;
 
-        hp -= damage;
-        Debug.Log("Моб отримав шкоду: " + damage + ". Поточне HP: " + hp);
+		hp -= damage;
+		Debug.Log("Моб отримав шкоду: " + damage + ". Поточне HP: " + hp);
 
-        if (hp <= 0)
-        {
-            isDead = true;
-            PlayerMonety.instance.AddMoney(coinsToDie); 
-            navmesh.Instance.RemoveEnemy(gameObject);
-            Destroy(gameObject);
-            return true;
-        }
+		if (hp <= 0)
+		{
+			isDead = true;
+			PlayerMonety.instance.AddMoney(coinsToDie);
+			navmesh.Instance.RemoveEnemy(gameObject);
+			Destroy(gameObject);
+			return true;
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    public void KillSilently()
-    {
-        if (isDead) return;
+	public void KillSilently()
+	{
+		if (isDead) return;
 
-        isDead = true;
-        navmesh.Instance.RemoveEnemy(gameObject);
-        Destroy(gameObject); 
-    }
+		isDead = true;
+		navmesh.Instance.RemoveEnemy(gameObject);
+		Destroy(gameObject);
+	}
 }
